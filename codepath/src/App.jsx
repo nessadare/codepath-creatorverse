53cc1fafd30e09f7daa637d3e05cb041a244ea49
vanessa <vanessadare12@gmail.com>
2025-08-14 22:21:47 +0100

    feat: view all content creators

--- codepath/src/App.jsx
+++ codepath/src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { useRoutes } from "react-router";
+import { Router, useRoutes } from "react-router";
 import ViewCreator from "./pages/ViewCreator";
 import AddCreator from "./pages/AddCreator";
-import ShowCreators from "./pages/ShowCreators";
 import EditCreator from "./pages/EditCreator";
+import { supabase } from "./client";
+import { createClient } from "@supabase/supabase-js";
+import CreatorsList from "./pages/CreatorsList";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [creators, setCreators] = useState([]);
 
-  // define paths for pages
+  // paths
 
   let element = useRoutes([
     {
       path: "/",
-      element: <ShowCreators />, // home page
+      element: <CreatorsList creators={creators} />,
       children: [
         {
           path: "edit",
@@ -33,6 +35,23 @@ function App() {
       ],
     },
   ]);
+
+  useEffect(() => {
+    const getCreators = async () => {
+      try {
+        const { data, error } = await supabase.from("creators").select(); // select all
+
+        if (error) throw error;
+
+        setCreators(data || []); // [] if data is null
+      } catch (err) {
+        console.log("Failure to get creators:", err);
+        alert("Failed to get creators.");
+      }
+    };
+
+    getCreators();
+  }, []);
 
   return element;
 }
---
+++ codepath/src/pages/CreatorsList.jsx
@@ -0,0 +1,23 @@
+// show all content creators
+
+import { useEffect } from "react";
+import Creator from "../components/Creator";
+
+const CreatorsList = (creators) => {
+  return (
+    <div className="cards">
+      <h1>List of all Creators</h1>
+      {creators && creators.length > 0 ? ( // handles empty and null arrays
+        <ul>
+          {creators.map((creator) => (
+            <Creator key={creator.id} creator={creator} /> // React requires key for efficiently updating lists
+          ))}
+        </ul>
+      ) : (
+        <p>No creators found</p>
+      )}
+    </div>
+  );
+};
+
+export default CreatorsList;
